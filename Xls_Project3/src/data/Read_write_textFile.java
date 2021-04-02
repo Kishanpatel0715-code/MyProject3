6a4eb858bbd17848fdfe24552613ae527ce814d4
kishan <patelkishan0715@gmail.com>
2021-04-02 12:48:12 -0400

    added some features into textfile

--- Xls_Project3/src/data/Read_write_textFile.java
+++ Xls_Project3/src/data/Read_write_textFile.java
@@ -27,6 +27,8 @@ public class Read_write_textFile {
 		rd.write("This is line 5");
 		rd.newLine();
 		rd.write("This is line 6");
+		rd.newLine();
+		rd.write("This is line 7");
 		rd.flush();
 		
 		
